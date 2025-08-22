035e52b471658a45e88d26aff95c0230f3acd54c
alagache <alagache@student.42.fr>
2025-08-22 22:33:46 +0200

    load safe tensor OK
    error loading state_dict into model

--- compile_small.py
+++ compile_small.py
@@ -33,12 +33,12 @@ from json import load
 # make a tinygrad implementation to compile
 class SmallTiny:
     def __init__(self, classes):
-        self.features: list[Tensor] = [
+        self.features: list[Callable[[Tensor], Tensor]] = [
             nn.Conv2d(3,32, 3), Tensor.relu, Tensor.max_pool2d,
             nn.Conv2d(32,64, 3), Tensor.relu, Tensor.max_pool2d,
         ]
 
-        self.classifier: list[Tensor] = [lambda x: x.flatten(1), nn.Linear(64 * 54 * 54, classes)]
+        self.classifier: list[Callable[[Tensor], Tensor]] = [lambda x: x.flatten(1), nn.Linear(64 * 54 * 54, classes)]
     
     def __call__(self, x:Tensor) -> Tensor:
         x = x.sequential(self.features)
@@ -47,20 +47,20 @@ class SmallTiny:
 
 if __name__ == "__main__":
     Device.DEFAULT = "WEBGPU"
+    # Device.DEFAULT = "CUDA"
 
     classes = None
     with open("SmallModel-images_dataset-Epch_10-Acc_92.json", "r") as f:
         classes = load(f)
 
     model = SmallTiny(len(classes))
-    print(model.features, model.classifier)
 
     state_dict = safe_load("SmallModel-images_dataset-Epch_10-Acc_92.safetensors")
+    # key error on claddifier 1 weight
     ret = load_state_dict(model, state_dict)
 
     inputs: Tensor = Tensor.randn(1, 3, 640, 640) # model input likeness
 
-    print(model.features, model.classifier)
     exit(0)
 
     # ensure safetensors and model works
